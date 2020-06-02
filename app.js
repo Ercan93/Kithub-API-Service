71044046695ab62c0eadad80b958913bfa46a9ef
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-02 13:58:08 +0300

    Create book.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var booksRouter = require('./routes/book');
 
 var app = express();
 
@@ -19,6 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 module.exports = app;
---
+++ routes/book.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+const Book = require('../models/BookSchema')
+
+
+
+module.exports = router;
