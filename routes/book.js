5755a555dd6519214b299db4036e7b92627088c8
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-02 13:59:35 +0300

    GET book with book name.

--- routes/book.js
+++ routes/book.js
@@ -3,5 +3,17 @@ var router = express.Router();
 const Book = require('../models/BookSchema')
 
 
+/* GET book with book name. */
+router.get('/:book_name', function (req, res, next) {
+    let bookName = req.params.book_name
+    bookName = bookName.replace("_", " ")
+    Book.find({ "properties.bookName": bookName })
+        .then(
+            data => res.json(data)
+        )
+        .catch(
+            err => res.json(err)
+        )
+});
 
 module.exports = router;
