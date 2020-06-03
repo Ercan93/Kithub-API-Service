6cb3b31eae2e7ae37de7927716cd94803612e3eb
Ercan Uzunsakal <ercan112358@gmail.com>
2020-06-03 21:33:10 +0300

    Update User register method.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt')
 const User = require('../models/UserSchema')
 
 /* GET home page. */
@@ -7,11 +8,17 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Kitap API Hizmetine Hoşgeldiniz' });
 });
 
+/* POST User register method */
 router.post('/register', (req, res, next) => {
     const { username, password } = req.body;
-    const user = new User({ username, password });
-    user.save()
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
+    bcrypt.hash(password, 10).then(hash => {
+        const user = new User({
+            username,
+            password: hash
+        });
+        user.save()
+            .then(data => res.json(data))
+            .catch(err => res.json(err))
+    })
 })
 module.exports = router;
