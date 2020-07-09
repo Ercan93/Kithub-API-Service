6d9843fd0b27494f3c7f06817ad647c1b0bcf90c
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-09 22:58:56 +0300

    [GET] Sort categories books by parameter values.

--- routes/category.js
+++ routes/category.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const Book = require("../models/BookSchema");
+
+const subRouter = express.Router({ mergeParams: true });
+router.use("/:category_name/sort", subRouter);
+
 /* GET category books with category name */
 router.get("/:category_name", (req, res) => {
   let categoryName = req.params.category_name;
@@ -13,4 +17,31 @@ router.get("/:category_name", (req, res) => {
     .catch((err) => res.json(err));
 });
 
+/* GET Sort categories books by parameter values. */
+subRouter.get("/:sort_param/:orderBy", (req, res) => {
+  let categoryName = req.params.category_name;
+  categoryName = categoryName.replace("_", " ");
+  let sort_param = req.params.sort_param;
+  let orderBy = req.params.orderBy;
+  orderBy = parseInt(orderBy);
+
+  if (
+    ["readCount", "likeCount", "pageNum"].includes(sort_param) &&
+    [-1, 1].includes(orderBy)
+  ) {
+    let sortQuery = {};
+    sort_param = "properties." + sort_param;
+    sortQuery[sort_param] = orderBy;
+
+    Book.find({
+      "properties.Category": { $in: [categoryName, " " + categoryName] },
+    })
+      .sort(sortQuery)
+      .limit(30)
+      .then((data) => res.json(data))
+      .catch((err) => res.json(err));
+  } else {
+    res.json("False sort parameter!");
+  }
+});
 module.exports = router;
