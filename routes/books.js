97afc4594f00f1e3a7bddd3d9e1c0a9054ddf1b6
Ercan Uzunsakal <ercan112358@gmail.com>
2020-07-19 10:49:44 +0300

    [ GET ] book with ISBN number endpoint prepared.

--- routes/books.js
+++ routes/books.js
@@ -1,25 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const Book = require('../models/BookSchema')
-
+const Book = require("../models/BookSchema");
 
 /* GET book with book name. */
-router.get('/:book_name', (req, res) => {
-    let bookName = req.params.book_name
-    bookName = bookName.replace("_", " ")
-    Book.find({ "properties.bookName": bookName })
-        .then(
-            data => res.json(data)
-        )
-        .catch(
-            err => res.json(err)
-        )
+router.get("/:book_name", (req, res) => {
+  let bookName = req.params.book_name;
+  bookName = bookName.replace("_", " ");
+  Book.find({ "properties.bookName": bookName })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
 });
 
-/* GET book with book Id. */
-router.get('/Id/:book_id', (req, res) => {
-    Book.findById(req.params.book_id)
-        .then(data => res.json(data))
-        .catch(err => res.json(err))
-})
+/* GET Book with ISBN number. */
+router.get("/ISBN/:book_ISBN", (req, res) => {
+  let ISBN = req.params.book_ISBN;
+  Book.find({ "properties.ISBN": ISBN })
+    .then((data) => res.json(data))
+    .catch((err) => res.json(err));
+});
 module.exports = router;
